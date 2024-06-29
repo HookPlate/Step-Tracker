0103603822e4ba1abb92028f6342207a2e175298
HookPlate <41370249+HookPlate@users.noreply.github.com>
2024-06-29 16:43:31 +0100

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by robin tetley on 29/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
