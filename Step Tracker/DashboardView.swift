16d66c1db93f9e0453d20b5839f031e98cc68b30
HookPlate <41370249+HookPlate@users.noreply.github.com>
2024-07-15 20:14:16 +0100

    step bar chart interactivity

--- Step Tracker/DashboardView.swift
+++ Step Tracker/DashboardView.swift
@@ -28,6 +28,7 @@ struct DashboardView: View {
     //is stashed in to userdefaults so it persists between app launches
     @AppStorage("hasSeenPermissionPriming") private var hasSeenPermissionPriming = false
     @State private var isShowingPermissionPrimingSheet = false
+    @State private var rawSelectedDate: Date?
     
     @State private var selectedStat: HealthMetricContext = .steps
     var isSteps: Bool { selectedStat == .steps }
@@ -36,6 +37,12 @@ struct DashboardView: View {
         guard !hkManager.stepData.isEmpty else {return 0}
         let totalSteps = hkManager.stepData.reduce(0) {$0 + $1.value}
         return totalSteps/Double(hkManager.stepData.count)
+    }
+    
+    var selectedHealthMetric: HealthMetric? {
+        guard let rawSelectedDate else {return nil}
+        return hkManager.stepData.first { Calendar.current.isDate(rawSelectedDate, inSameDayAs: $0.date)
+        }
     }
     
     var body: some View {
@@ -74,6 +81,17 @@ struct DashboardView: View {
                         .padding(.bottom, 12)
                         
                         Chart {
+                            //if they have their finger on the chart
+                            if let selectedHealthMetric {
+                                RuleMark(x: .value("Selected Metric", selectedHealthMetric.date, unit: .day))
+                                    .foregroundStyle(Color.secondary.opacity(0.3))
+                                    .offset(y: -10)
+                                    .annotation(position: .top,
+                                                spacing: 0,
+                                                overflowResolution: .init(x: .fit(to:.chart), y: .disabled)) {
+                                        annotationView
+                                    }
+                            }
                             //this is a viewbuilder, think of it as a zstack
                             RuleMark(y: .value("Average", avStepCount))
                                 .foregroundStyle(Color.secondary)
@@ -84,9 +102,13 @@ struct DashboardView: View {
                                     y: .value("Steps", steps.value)
                                 )
                                 .foregroundStyle(Color.pink.gradient)
+                                //so when the finger is on the screen give the bar iunder it 1.0, of if nothing, give them all that.
+                                .opacity(rawSelectedDate == nil || steps.date == selectedHealthMetric?.date ? 1.0 : 0.3)
                             }
                         }
                         .frame(height: 150)
+                        //when this property changes we want to aniomate those changes. The modifier tracks where the person is dragging and gives us the date.
+                        .chartXSelection(value: $rawSelectedDate.animation(.easeInOut))
                         .chartXAxis {
                             AxisMarks {
                                 AxisValueLabel(format: .dateTime.month(.defaultDigits).day())
@@ -141,7 +163,24 @@ struct DashboardView: View {
         }
         .tint(isSteps ? .pink : .indigo)
     }
-        
+    var annotationView: some View {
+        VStack(alignment: .leading) {
+            Text(selectedHealthMetric?.date ?? .now, format:
+                    .dateTime.weekday(.abbreviated).month(.abbreviated).day())
+                    .font(.footnote.bold())
+                    .foregroundStyle(.secondary)
+            
+            Text(selectedHealthMetric?.value ?? 0, format: .number.precision(.fractionLength(0)))
+                .fontWeight(.heavy)
+                .foregroundStyle(.pink)
+        }
+        .padding(12)
+        .background(
+            RoundedRectangle(cornerRadius: 4)
+                .fill(Color(.secondarySystemBackground))
+                .shadow(color: .secondary.opacity(0.3), radius: 2, x: 2, y: 2)
+        )
+    }
 }
 
 #Preview {
